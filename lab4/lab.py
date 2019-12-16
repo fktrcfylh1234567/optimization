e697b06092b63828a4fcb90c116efa2643f004b8
fktrcfylh1234567 <fktrcfylh1234567@yandex.ru>
2019-12-16 12:47:46 +0300

    add Pareto choice

--- lab4/lab.py
+++ lab4/lab.py
@@ -1,13 +1,17 @@
-from lab4 import main_criteria
+from lab4 import mko
 
 mgts = (2, 5, 5, 3)
 rustelecom = (1, 3, 3, 4)
 akado = (6, 2, 4, 2)
 qwerty = (5, 4, 4, 5)
 choices = (mgts, rustelecom, akado, qwerty)
+labels = ("МГТС", "Ростелеком", "Акадо", "QWERTY")
 
 limits = (0.5, 0.2, 0.5)
-main_criteria_choice = main_criteria.choice(choices, 0, limits)
-print("main criteria choice:", main_criteria_choice + 1)
+main_criteria_choice = mko.main_criteria(choices, 0, limits)
+print("main criteria choice:", labels[main_criteria_choice])
+
+pareto_choice = mko.pareto(choices, 0, 2, labels)
+print("pareto choice:", labels[pareto_choice])
 
 weights = (2, 8, 4, 6)
--- lab4/mko.py
+++ lab4/mko.py
@@ -1,7 +1,8 @@
 from copy import copy
+import matplotlib.pyplot as plt
 
 
-def choice(choices, main, limits):
+def main_criteria(choices, main, limits):
     maximums = list(max(choices, key=lambda a: a[i])[i] for i in range(4) if i != main)
     limits_num = list(maximums[i] * limits[i] for i in range(3))
     limits_num.insert(main, None)
@@ -12,3 +13,13 @@ def choice(choices, main, limits):
 
     opt_choice = choices.index(max(filtered, key=lambda a: a[main]))
     return opt_choice
+
+
+def pareto(choices, main_x, main_y, labels):
+    for i in range(4):
+        plt.plot(choices[i][main_x], choices[i][main_y], 'o', label=labels[i])
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    return choices.index(max(choices, key=lambda a: a[main_x] ** 2 + a[main_y] ** 2))
