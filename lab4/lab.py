acc27799d60ebc29505eb3c6ef49ce98213f7991
fktrcfylh1234567 <fktrcfylh1234567@yandex.ru>
2020-01-22 10:12:45 +0300

    add consistency

--- lab4/lab.py
+++ lab4/lab.py
@@ -2,8 +2,8 @@ from lab4 import pareto, main_criteria, saaty, convolution
 
 mgts = (5, 5, 5, 3)
 rustelecom = (6, 3, 3, 4)
-akado = (1, 2, 4, 2)
-qwerty = (2, 4, 4, 5)
+akado = (2, 2, 4, 2)
+qwerty = (3, 4, 4, 5)
 choices = (mgts, rustelecom, akado, qwerty)
 labels = ("МГТС", "Ростелеком", "Акадо", "QWERTY")
 dirs = (-1, 1, 1, 1)
@@ -19,5 +19,5 @@ weights = (-2, 8, 4, 6)
 convolution_choice = convolution.convolution(choices, weights)
 print("convolution choice:", labels[convolution_choice])
 
-saaty_choice = saaty.saaty(choices, weights)
+saaty_choice = saaty.saaty(choices, weights, labels)
 print("Saaty choice:", labels[saaty_choice])
--- lab4/saaty.py
+++ lab4/saaty.py
@@ -1,7 +1,12 @@
+from functools import reduce
+
 import numpy as np
+from prettytable import PrettyTable
 
 
-def saaty(choices, weights):
+def saaty(choices, weights, labels):
+    consistency(choices, labels)
+
     choices = [[sum(choices[i][cr] / choices[j][cr] for j in range(4)) for i in range(4)] for cr in range(4)]
     choices = np.array(choices).transpose()
     choices = choices / np.abs(choices).sum(axis=0)
@@ -12,3 +17,40 @@ def saaty(choices, weights):
 
     res = np.dot(choices, weights)
     return np.where(res == np.amax(res))[0][0]
+
+
+def consistency(choices, labels):
+    pretty_table = PrettyTable()
+    pretty_table.field_names = ["Choice", labels[0], labels[1], labels[2], labels[3]]
+
+    for crit in range(4):
+        matrix = [[choices[i][crit] / choices[j][crit] for j in range(4)] for i in range(4)]
+
+        for i in range(4):
+            row = [labels[i]]
+            row += matrix[i]
+            pretty_table.add_row(row)
+
+        print(pretty_table)
+
+        matrix = np.array(matrix)
+
+        # Own vectors
+        vecs = [reduce(lambda a, b: a * b, matrix[i]) ** (1 / 4) for i in range(4)]
+
+        # Sums
+        matrix = matrix.transpose()
+        s = np.array([sum(matrix[i]) for i in range(4)])
+
+        # Normalizing own vectors
+        vecs_norm = [vecs[i] / s[i] for i in range(4)]
+
+        # Own value
+        own_val = sum(s * vecs_norm)
+
+        consistency_index = (own_val - 4) / 3
+        consistency_ratio = consistency_index / 0.9
+
+        print("consistency ratio:", consistency_ratio)
+        pretty_table.clear_rows()
+        print()
